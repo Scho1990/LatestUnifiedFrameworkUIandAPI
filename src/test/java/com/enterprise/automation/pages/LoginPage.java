f65b76486ee7d3f217ebbe72e636b8694425a4f0
Scho1990 <it.santosh68@gmail.com>
2026-06-12 12:17:33 +0530

    deliberatly failing test case for checking screenshot in allure during jenkins and github actions cicd check

--- src/test/java/com/enterprise/automation/pages/LoginPage.java
+++ src/test/java/com/enterprise/automation/pages/LoginPage.java
@@ -8,7 +8,7 @@ public class LoginPage extends BasePage {
     private final By usernameInput = By.id("user-name");
     private final By passwordInput = By.id("password");
     private final By loginButton = By.id("login-button");
-    private final By errorMessage = By.cssSelector("[data-test='error']");
+    private final By errorMessage = By.cssSelector("[data-test='error1']");
 
     public boolean isLoginButtonDisplayed() {
         return WaitUtils.waitForVisible(loginButton).isDisplayed();
